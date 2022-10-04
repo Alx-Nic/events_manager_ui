fbbc80fb20b035af41571d386d1977066ca1eb7a
Alexandru Nicula <nicula.alexandru@live.com>
2022-10-04 18:15:49 +0200

    Email confirmed field added

--- src/components/AccessUserCard.vue
+++ src/components/AccessUserCard.vue
@@ -16,11 +16,13 @@
         <div>email: <b>{{accessUser.email}}</b></div>
         <div>phone number: <b>{{accessUser.phoneNumber}}</b></div>
         <div>role: <b>{{accessUser.role}}</b></div>
+        <div>email confirmed: <b>{{accessUser.emailVerified}}</b></div>
       </v-card-text>
-      <v-card-actions>        
+      <v-card-actions>   
         <v-btn color="primary" tile small>edit user</v-btn>
         <v-btn color="secondary" tile x-small>delete user</v-btn>
         <v-btn color="secondary" tile x-small >reset password</v-btn>
+        
       </v-card-actions>
     </v-card>
 </template>
